15e5aaeb29704de6649b3572a418d1de4ff9a769
Mochamad Noor Syamsu <samsu.noor@gmail.com>
2021-03-04 22:09:46 +0700

    chore: failed to center shiet :(

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="columns is-centered">
+    <div className="columns is-mobile is-centered">
       <div className="column is-narrow">
-        <form>
-          <label>Username: <input /></label>
-          <br/>
-          <label>Password: <input type="password" /></label>
-          <br/>
+        <form className="box has-centered">
+          <label>
+            Username <input />
+          </label>
+          <br />
+          <label>
+            Password <input type="password" />
+          </label>
+          <br />
           <button className="button is-primary">LOGIN</button>
         </form>
       </div>
