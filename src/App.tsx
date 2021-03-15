350e1675f8247a0d7bb3f2157e12fc6135da149d
Mochamad Noor Syamsu <samsu.noor@gmail.com>
2021-03-15 19:38:42 +0700

    implement react useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+function App({}) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const onChangeUsername = (event) => {
+    setUsername(event.target.value);
+    console.log("username changed: " + username);
+  };
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+    console.log("password changed: " + password);
+  };
   return (
     <section className="hero is-info is-fullheight">
       <div className="hero-body container">
         <div className="box field">
           <label className="label">Username</label>
           <div className="control">
-            <input className="input" />
+            <input className="input" onChange={onChangeUsername} />
           </div>
           <br />
           <label className="label">Password</label>
           <div className="control">
-            <input className="input" type="password" />
+            <input
+              className="input"
+              onChange={onChangePassword}
+              type="password"
+            />
           </div>
           <br />
           <div className="control has-text-centered">
