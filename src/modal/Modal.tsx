12ac973000bed7a6b7580321f2a4a780cf438261
Mochamad Noor Syamsu <samsu.noor@gmail.com>
2021-05-20 16:10:53 +0700

    wrong container portal

--- src/modal/Modal.tsx
+++ src/modal/Modal.tsx
@@ -1,34 +1,10 @@
-import React, { FunctionComponent, useEffect } from "react";
+import { FunctionComponent } from "react";
 import ReactDOM from "react-dom";
 
-const modalRoot = document.getElementById("modal-root") as HTMLElement;
-// assuming in your html file has a div with id 'modal-root';
-
-const ModalComponent: FunctionComponent = ({ children }) => {
-  const el: HTMLElement = document.createElement("div");
-  useEffect(() => {
-    document.body.appendChild(el);
-    return () => {
-      document.body.removeChild(el);
-    };
-  });
-  return ReactDOM.createPortal(children, el);
+const Modal: FunctionComponent = ({ children }) => {
+  const modalRoot = document.getElementById("modal-root") as HTMLElement;
+  // assuming in your html file has a div with id 'modal-root';
+  return ReactDOM.createPortal(children, modalRoot);
 };
 
-class Modal extends React.Component {
-  el: HTMLElement = document.createElement("div");
-
-  componentDidMount() {
-    modalRoot.appendChild(this.el);
-  }
-
-  componentWillUnmount() {
-    modalRoot.removeChild(this.el);
-  }
-
-  render() {
-    return ReactDOM.createPortal(this.props.children, this.el);
-  }
-}
-
-export { ModalComponent, Modal };
+export { Modal };
