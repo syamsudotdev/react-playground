0f5ae77121061183e869dc362bfdcc14530753b3
Mochamad Noor Syamsu <samsunoor@users.noreply.github.com>
2021-05-20 16:11:41 +0700

    show modal with portal (#3)
    * try to refactor modal to functional
    
    * try to use useEffect
    
    * wrong container portal

--- src/modal/Modal.tsx
+++ src/modal/Modal.tsx
@@ -1,21 +1,10 @@
-import React from "react";
+import { FunctionComponent } from "react";
 import ReactDOM from "react-dom";
 
-const modalRoot = document.getElementById("modal-root") as HTMLElement;
-// assuming in your html file has a div with id 'modal-root';
+const Modal: FunctionComponent = ({ children }) => {
+  const modalRoot = document.getElementById("modal-root") as HTMLElement;
+  // assuming in your html file has a div with id 'modal-root';
+  return ReactDOM.createPortal(children, modalRoot);
+};
 
-export class Modal extends React.Component {
-  el: HTMLElement = document.createElement("div");
-
-  componentDidMount() {
-    modalRoot.appendChild(this.el);
-  }
-
-  componentWillUnmount() {
-    modalRoot.removeChild(this.el);
-  }
-
-  render() {
-    return ReactDOM.createPortal(this.props.children, this.el);
-  }
-}
+export { Modal };
